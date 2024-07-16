ce56e8e363279d936315edf9b40afc3f18057d4a
nyutu_grace <nyutu_grace15@gmail.com>
2024-07-16 01:04:00 +0100

    allow composer merge of backgrounds

--- src/image_composer.py
+++ src/image_composer.py
@@ -2,10 +2,8 @@ from typing import List, Literal, Tuple
 import itertools
 import random
 from collections import defaultdict
-
 from PIL import Image
 from pprint import pprint
-
 
 VERTICAL_POSITIONING = {'Logo': [1], 'CTA Button': [1, 2, 3], 'Icon': [1, 2, 3], 'Product Image': [2],
                'Text Elements': [1,3], 'Infographic': [2], 'Banner': [1], 'Illustration': [2], 'Photograph': [2],
@@ -22,15 +20,19 @@ HORIZONTAL_POSITIONING = {'Logo': [1], 'CTA Button': [2, 1, 3], 'Icon': [1], 'Pr
                           'Legal Disclaimers or Terms': [3], 'Contact Information': [3, 1, 2],
                           'Map or Location Image': [3], 'QR Code': [3, 1, 2]}
 
-
 class ImageComposer:
-    categories = Literal["Background", "Logo", "CTA Button", "Icon", "Product Image", "Text Elements", "Infographic", "Banner", "Illustration", "Photograph", "Mascot", "Testimonial Quotes", "Social Proof", "Seal or Badge", "Graphs and Charts", "Decorative Elements", "Interactive Elements", "Animation", "Coupon or Offer Code", "Legal Disclaimers or Terms", "Contact Information", "Map or Location Image", "QR Code"]
+    categories = Literal[
+        "Background", "Logo", "CTA Button", "Icon", "Product Image", "Text Elements", "Infographic", "Banner", 
+        "Illustration", "Photograph", "Mascot", "Testimonial Quotes", "Social Proof", "Seal or Badge", 
+        "Graphs and Charts", "Decorative Elements", "Interactive Elements", "Animation", "Coupon or Offer Code", 
+        "Legal Disclaimers or Terms", "Contact Information", "Map or Location Image", "QR Code"
+    ]
     PositionSegment = Tuple[float, float]
     AlignmentPosition = Tuple[int, int]
     AlignmentPositions = List[AlignmentPosition]
-    frame_images = List[Tuple[categories, str, str]]
+    frame_images = List[Tuple[str, str, str]]
 
-    def __init__(self, width:int, height: int, frames: List[frame_images]) -> None:
+    def __init__(self, width: int, height: int, frames: List[frame_images]) -> None:
         self.width = width
         self.height = height
         self.frames = frames
@@ -45,22 +47,37 @@ class ImageComposer:
         self.generated_frames = []
 
         for frame in self.frames:
-            # Separate Background
             placement_items = []
-            for index, item in enumerate(frame):
-                if item[0] == "Background":
-                    background_index = index
-                    continue
-                placement_items.append(item)
-            
-            background = frame[background_index]
+            background_items = []
 
-            possibilties = ImageComposer.compute_positions([item[0] for item in placement_items])
-            identified_locations = ImageComposer.select_diverse_positions(possibilties)
+            for item in frame:
+                if item[0].lower() == "background":  # Ensure background check is case-insensitive
+                    background_items.append(item)
+                else:
+                    # Check if the item has exactly three elements
+                    if len(item) != 3:
+                        raise ValueError(f"Item {item} does not have exactly three elements")
+                    placement_items.append(item)
+            
+            if not background_items:
+                raise ValueError("No background found in frame")
+
+            possibilities = ImageComposer.compute_positions([item[0] for item in placement_items])
+            identified_locations = ImageComposer.select_diverse_positions(possibilities)
             adjusted_positions = self.calculate_adjusted_element_positions(identified_locations)
-            placement_values = [(x[2], *list(y.values())) for x, y in zip(placement_items, adjusted_positions)]
-            # Construct Frame
-            self.generated_frames.append(self.create_combined_image(background[2], placement_values))
+            
+            # Debugging prints
+            print("Frame:", frame)
+            print("Placement Items:", placement_items)
+            print("Identified Locations:", identified_locations)
+            print("Adjusted Positions:", adjusted_positions)
+
+            # Ensure category is included in placement_values
+            placement_values = [(x[2], (y['x_start'], y['y_start']), (y['width'], y['height']), x[0]) for x, y in zip(placement_items, adjusted_positions)]
+            
+            for background in background_items:
+                # Construct Frame for each background
+                self.generated_frames.append(self.create_combined_image(background[2], placement_values))
 
     @staticmethod
     def compute_positions(elements: List[categories]) -> List[AlignmentPositions]:
@@ -97,7 +114,6 @@ class ImageComposer:
         
         return selected_positions
 
-
     @staticmethod
     def get_image_position_segments(width: float, height: float, vm: float = 0.6, vo: float = 0.2, hm: float = 0.6, ho: float = 0.2) -> Tuple[List[PositionSegment], List[PositionSegment]]:
         """Divide Image based on percentage for vertical and horizontal segments."""
@@ -122,117 +138,71 @@ class ImageComposer:
             (horizontal_outer + horizontal_mid, width)
         ]
 
-        segements = []
-        for vs in vertical_segments:
-            vs_items = []
-            for hs in horizontal_segments:
-                vs_items.append((vs, hs))
-            segements.append(vs_items)
+        return vertical_segments, horizontal_segments
 
+    def calculate_adjusted_element_positions(self, positions: AlignmentPositions) -> List[dict]:
+        adjusted_positions = []
 
-        return segements
-    
-    def calculate_adjusted_element_positions(self, elements_positions, padding=10):
-        element_details = []
-        segment_elements = {}
+        for pos in positions:
+            vertical_segment = self.segments[0][pos[0] - 1]
+            horizontal_segment = self.segments[1][pos[1] - 1]
 
-        # Organize elements by their segments
-        for i, (v_pos, h_pos) in enumerate(elements_positions):
-            segment_key = (v_pos, h_pos)
-            if segment_key not in segment_elements:
-                segment_elements[segment_key] = []
-            segment_elements[segment_key].append(i)
-        
-        for segment_key, elements in segment_elements.items():
-            v_pos, h_pos = segment_key
-            segment = self.segments[v_pos-1][h_pos-1]
-            vertical_segment, horizontal_segment = segment
-            num_elements = len(elements)
-            
-            x_start, x_end = horizontal_segment
-            y_start, y_end = vertical_segment
-            segment_width = (x_end - x_start) - 2 * padding
-            segment_height = (y_end - y_start) - 2 * padding
-            
-            # Determine alignment and divide space
-            is_vertical = segment_height > segment_width
-            if is_vertical:
-                space_per_element = segment_height / num_elements
-            else:
-                space_per_element = segment_width / num_elements
-            
-            for index, _ in enumerate(elements):
-                if is_vertical:
-                    element_x_start = x_start + padding
-                    element_y_start = y_start + padding + index * space_per_element
-                    element_width = segment_width
-                    element_height = space_per_element
-                else:
-                    element_x_start = x_start + padding + index * space_per_element
-                    element_y_start = y_start + padding
-                    element_width = space_per_element
-                    element_height = segment_height
-                
-                element_details.append({
-                    "start_point": (element_x_start, element_y_start),
-                    "dimensions": (element_width, element_height)
-                })
+            x_start = horizontal_segment[0]
+            y_start = vertical_segment[0]
+            width = horizontal_segment[1] - horizontal_segment[0]
+            height = vertical_segment[1] - vertical_segment[0]
 
-        return element_details
-    
-    @staticmethod
-    def resize_image(image, target_width, target_height):
-        """
-        Resize an image to fit within target dimensions while maintaining aspect ratio.
-        """
-        original_width, original_height = image.size
-        ratio = min(target_width / original_width, target_height / original_height)
-        new_width = int(original_width * ratio)
-        new_height = int(original_height * ratio)
-        resized_image = image.resize((new_width, new_height), Image.ANTIALIAS)
-        return resized_image
+            adjusted_positions.append({
+                'x_start': x_start,
+                'y_start': y_start,
+                'width': width,
+                'height': height
+            })
 
-    def create_combined_image(self, background_path: str, elements: List[Tuple[str, int|float, int|float]]) -> Image.Image:
+        return adjusted_positions
+
+    def create_combined_image(self, background_path: str, elements_positions: list) -> Image.Image:
         """
         Create a combined image based on background and elements' positioning and sizing.
         
         :param background_path: Path to the background image.
-        :param elements: A list of dictionaries, each containing 'image_path', 'start_point', and 'dimensions'.
+        :param elements_positions: A list of tuples containing 'image_path', 'start_point', and 'dimensions'.
         """
-        # Load the background image
-        background = Image.open(background_path).convert("RGBA")
-        
-        for element in elements:
-            # Load element image
-            image_path = element[0]
-            image = Image.open(image_path).convert("RGBA")
-            
-            # Resize image according to dimensions without losing aspect ratio
-            target_width, target_height = element[2]
-            resized_image = ImageComposer.resize_image(image, target_width, target_height)
-            
-            # Calculate position to center the image within its segment
-            start_x, start_y = element[1]
-            offset_x = start_x + (target_width - resized_image.size[0]) / 2
-            offset_y = start_y + (target_height - resized_image.size[1]) / 2
-            
-            # Place the resized image on the background
-            background.paste(resized_image, (int(offset_x), int(offset_y)), resized_image)
-        
-        return background
-    
+        background_image = Image.open(background_path).convert("RGBA")
+        background_image = background_image.resize((self.width, self.height))
 
-if __name__ == "__main__":
-    ic = ImageComposer(320, 500, [[('Logo', 'url_path', 'local_path'), 
-                                   ('Call-To-Action (CTA) Button', 'url_path', 'local_path'),
-                                   ('Icon', 'url_path', 'local_path'),
-                                   ('Product Image', 'url_path', 'local_path'),
-                                   ('Text Elements', 'url_path', 'local_path')]])
-    possibilties = ImageComposer.compute_positions(["Logo", "Call-To-Action (CTA) Button", "Icon", "Product Image", "Text Elements"])
-    pprint(possibilties)
-    print("======================================================")
-    diverse = ImageComposer.select_diverse_positions(possibilties)
-    pprint(diverse)
+        print(f"Creating combined image with background: {background_path}")
+        print(f"Elements Positions: {elements_positions}")
 
-    print(ic.calculate_adjusted_element_positions(diverse))
-    
+        for element in elements_positions:
+            if len(element) != 4:
+                raise ValueError(f"Element {element} does not have exactly four components")
+
+            element_path, (x_start, y_start), (width, height), category = element
+            element_image = Image.open(element_path).convert("RGBA")
+            element_image = self.resize_image(element_image, int(width), int(height))
+
+            print(f"Placing element {element_path} at ({x_start}, {y_start}) with size ({width}, {height})")
+
+            background_image.paste(element_image, (int(x_start), int(y_start)), element_image)
+
+        return background_image
+
+    @staticmethod
+    def resize_image(image: Image.Image, target_width: int, target_height: int) -> Image.Image:
+        aspect_ratio = image.width / image.height
+        
+        if target_width / target_height > aspect_ratio:
+            new_height = target_height
+            new_width = int(target_height * aspect_ratio)
+        else:
+            new_width = target_width
+            new_height = int(target_width / aspect_ratio)
+
+        resized_image = image.resize((new_width, new_height), Image.LANCZOS)
+        return resized_image
+
+
+
+
+
